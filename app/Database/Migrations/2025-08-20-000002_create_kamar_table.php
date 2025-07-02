382a45106b13e583fa4269032cd36cd98f7bbf86
Fauzan Adli <fauzan.adli92@gmail.com>
2025-07-02 10:05:54 +0700

    kamar

--- app/Database/Migrations/2025-08-20-000002_create_kamar_table.php
+++ app/Database/Migrations/2025-08-20-000002_create_kamar_table.php
@@ -5,12 +5,12 @@ namespace App\Database\Migrations;
 use CodeIgniter\Database\Migration;
 use PHPSQLParser\builders\PrimaryKeyBuilder;
 
-class CreateTamuTable extends Migration
+class CreateKamarTable extends Migration
 {
     public function up()
     {
         $this->forge->addField([
-            'nik' => [
+            'id' => [
                 'type'           => 'INT',
                 'constraint'     => 11,
                 'PrimaryKey' => true,
@@ -20,21 +20,13 @@ class CreateTamuTable extends Migration
                 'type'       => 'VARCHAR',
                 'constraint' => 100,
             ],
-            'alamat' => [
-                'type'       => 'TEXT',
-                'constraint' => 100,
+            'harga' => [
+                'type'       => 'DOUBLE',
+                'constraint' => '10,2',
             ],
-            'nohp' => [
-                'type'       => 'INT',
-                'constraint' => 11,
-            ],
-            'jenkel' => [
+             'status_kamar' => [
                 'type'       => 'ENUM',
-                'constraint' => ['L', 'P'],
-            ],
-            'iduser' => [
-                'type'       => 'INT',
-                'constraint' => 11,
+                'constraint' => [1, 2],
             ],
             'created_at' => [
                 'type' => 'DATETIME',
@@ -49,23 +41,22 @@ class CreateTamuTable extends Migration
                 'null' => true,
             ],
         ]);
-        $this->forge->addKey('nik', true);
-        $this->forge->createTable('tamu');
+        $this->forge->createTable('kamar');
+
+        // Tambahkan admin default
         $data = [
-            'nik'   => '1234567890',
-            'nama'   => 'rindi',
-            'alamat'      => 'Padang',
-            'tgl_lahir' => '2000-01-01',
-            'nohp'   => '09232323',
-            'jenkel'       => 'P',
+            'id'   => 1,
+            'nama'   => 'Kamar 101',
+            'harga'      => 500000,
+            'status_kamar' => 1,
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ];
-        $this->db->table('tamu')->insert($data);
+        $this->db->table('kamar')->insert($data);
     }
 
     public function down()
     {
-        $this->forge->dropTable('tamu');
+        $this->forge->dropTable('kamar');
     }
 }
